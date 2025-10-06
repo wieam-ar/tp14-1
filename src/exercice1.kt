2f4f3e0642ea26d8945571cb7578a93718a23bbb
wieam Ar <aarikawieam@gmail.com>
2025-10-06 12:25:37 +0100

    code cpourotine

--- src/exercice1.kt
+++ src/exercice1.kt
@@ -1,2 +1,35 @@
-class exercice1 {
+
+import kotlinx.coroutines.*
+
+suspend fun verifierDisponibilite() {
+    println(" Vérification des ingrédients...")
+    delay(2000)
+    println(" Ingrédients disponibles !")
+}
+
+suspend fun preparerCommande() {
+    println(" Préparation de la commande...")
+    delay(5000)
+    println(" Commande prête !")
+}
+
+suspend fun livrerRepas() = withContext(Dispatchers.IO) {
+    println(" Livraison du repas en cours...")
+    delay(3000)
+    println(" Repas livré avec succès !")
+}
+
+fun main() = runBlocking {
+    println("--- Gestion de commande du restaurant ---")
+
+    //  Vérifier la disponibilité (bloque jusqu’à la fin)
+    verifierDisponibilite()
+
+    // Préparer la commande (bloque jusqu’à la fin)
+    preparerCommande()
+
+    // Livrer le repas (exécutée dans le contexte IO)
+    livrerRepas()
+
+    println(" Toutes les étapes sont terminées avec succès !")
 }
